8165ba0b8c37e39d78c52bee89eb34837f539382
Oriol <bover73@gmail.com>
2019-09-18 17:08:25 +0200

    refactor styles 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Header } from './components/layout/Header'
 import { Sidebar } from './components/layout/Sidebar'
+import { Tasks } from './components/Tasks'
 
 export const App = () => (
   <div className='App'>
     <header className='App-header'>
       <Header />
       <Sidebar />
+      <Tasks />
     </header>
   </div>
 )
--- src/components/layout/Header/index.js
+++ src/components/layout/Header/index.js
@@ -5,14 +5,16 @@ export const Header = () => {
   return (
     <header className='header' data-testid='header'>
       <nav>
-        <img src='/images/logo.png' alt='todo logo' />
+        <div className='logo'>
+          <img src='/images/logo.png' alt='todo logo' />
+        </div>
+        <div className='settings'>
+          <ul>
+            <li data-testid='quick-add-task-action' className='settings__add'>+</li>
+            <li data-testid='dark-mode-action'><FaPizzaSlice /></li>
+          </ul>
+        </div>
       </nav>
-      <div className='settings'>
-        <ul>
-          <li>+</li>
-          <li><FaPizzaSlice /></li>
-        </ul>
-      </div>
     </header>
   )
 }
